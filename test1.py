65bca56320a1854664d2299e51968bf8bdde02c8
Franco Lehmann <francolehmann28@gmail.com>
2024-03-01 11:18:00 -0300

    Se agrego un poco mas de codigo

--- test1.py
+++ test1.py
@@ -1,4 +1,8 @@
 if __name__ == '__main__':
     
-    if 4>3:
+    print("Choose a number: ")
+    option = int(input())
+    if option>3:
      print('Hola Mundo')
+    else:
+       print("Adios Mundo")
