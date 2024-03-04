cb48246521b05aa4eb733346a511f8c449b358a2
Franco Lehmann <francolehmann28@gmail.com>
2024-03-04 15:32:11 -0300

    Se creo el primer servicio con FastAPI

--- main.py
+++ main.py
@@ -1,7 +1,16 @@
 import test1
+from fastapi import FastAPI
 
+app = FastAPI()
+
+@app.get('/')
+def getSource():
+    return [1,2,3,4,5,6,7,8,9]
+@app.get('/contact')
+def getSource():
+    return {'name':'Franco'}
 def run():
-    test1.getProducts()
+    return test1.getProducts()
 
 
 if __name__ == '__main__':
